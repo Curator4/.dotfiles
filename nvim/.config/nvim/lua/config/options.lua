45d91e636fc1c1571b0eb9e783f3cb95472b1b6a
curator <curator4@gmail.com>
2026-01-12 19:54:19 +0100

    testo

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -22,3 +22,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+-- Line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
